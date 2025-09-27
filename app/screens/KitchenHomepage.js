cb5f921880304aec767ef232766d03e47cf16f7e
Kristen Masters <kristen.masters01@student.csulb.edu>
2025-09-27 04:17:06 -0700

    Very small changes to KitchenHompage. Font colors and logo. -Kristen

--- app/screens/KitchenHomepage.js
+++ app/screens/KitchenHomepage.js
@@ -25,14 +25,14 @@ import {
 
 export default function KitchenHomepage({navigation}) {
 return (
-    <ImageBackground style={styles.background} source={require("../assets/IMG_1.jpg")}>
+    <ImageBackground style={styles.background}>
 
       <View style={styles.logoContainer}>
-        <Image source={require("../assets/ye.png")} style={styles.logo} />
+        <Image source={require("../assets/Just_Icon.png")} style={styles.logo} />
         <Text style={styles.title}>Cache Money Made</Text>
       </View>
       
-      <Text style={styles.description}>Cooking Crazy 4 U</Text>
+      <Text style={[styles.description,{position: 'relative', top:-30}]}>Cooking Crazy 4 U</Text>
 
       <View style ={styles.buttons}>
         
@@ -41,7 +41,7 @@ return (
         </TouchableOpacity>
 
         <TouchableOpacity style={styles.registerButton} onPress={() => navigation.navigate("Registration")}>
-          <Text style ={{fontSize: 24, color: "black"}}>Sign-Up</Text>
+          <Text style ={{fontSize: 24, color: "black",position: 'relative', bottom: 20, left: 15}}>Sign-Up</Text>
         </TouchableOpacity>
         </View>
     </ImageBackground>
@@ -55,13 +55,27 @@ const styles = StyleSheet.create({
         alignItems: "center"
     },
     loginButton: {
-        width: "100%",
-        height: 70,
-        backgroundColor: "#fc5c65"
+        width: 100,
+        height: 67,
+        backgroundColor: "#53B175",
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 19,
+        marginBottom: 18,
+        bottom: "15%",
+        // shadow for ios users
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 4},
+        shadowOpacity: 0.25,
+        shadowRadius: 3,
+        // shadow for android users
+        elevation: 3,
     },
     logo: {
-        width:100,
-        height: 100,
+        // width: 100,
+        // height: 100,
+        width: 200,
+        height: 200,
         position: 'absolute',
         top: 70,
     },
