c7e3e2d12455d1469947a548bad2186561b99690
BrandonC2 <98776493+BrandonC2@users.noreply.github.com>
2025-09-29 16:32:33 -0700

    created server file, connected to mongo

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,14 @@ app.use(express.json());
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
+const db = mongoose.connection;
+db.on('error', (err) => {
+  console.error('MongoDB connection error:', err);
+});
+db.once('open', () => {
+  console.log('Connected to MongoDB successfully!');
+});
+
 // Import routes
 // const userRoutes = require('./routes/userRoutes');
 // const inventoryRoutes = require('./routes/inventoryRoutes');
