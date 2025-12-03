c07287677b889b1fe7e3f0b831bf9a3bc53ce16a
Christian Bae <yewoookie@gmail.com>
2025-12-03 15:57:43 -0800

    package-losck and server.js commit

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const mongoose = require("mongoose");
 const http = require("http");
 const { Server } = require("socket.io");
 
+//To start server
+// cd backend
+// npm run dev
+
 // Express app setup
 const app = express();
 app.use(cors());
